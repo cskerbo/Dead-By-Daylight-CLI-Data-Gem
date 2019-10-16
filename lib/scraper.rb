fc1b57ca8f20eba7f12d907c4a8df88098f2a9d2
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-16 22:43:15 +0000

    Added scrape_perks method - WIP

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,6 @@ class Scraper
       name = survivor.css("a").attribute("title").text
       survivors << name
     end
-
   end
 
   def self.scrape_killers
@@ -25,7 +24,20 @@ class Scraper
       name = killer.css('a').text
       killers << name
     end
-
   end
 
+  def self.scrape_perks
+    page = Nokogiri::HTML(open("https://deadbydaylight.gamepedia.com/Perks"))
+    perks = []
+
+    perk_extract = page.css('div.mw-parser-output table.wikitable.sortable tr th')
+    perk_extract.each do |perk|
+      perks_isolated = perk.css('a').text
+      binding.pry
+        perks << perks_isolated
+
+    end
+puts perks
+  end
+scrape_perks
 end
