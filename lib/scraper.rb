478bf513103bf137ef2893c13bea8c89670cdec3
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-17 19:05:17 +0000

    Added logic to grab bio for scrape_survivors

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,10 +11,23 @@ class Scraper
     survivor_list = page.css('div[style*="flex:1"]')
     survivor_list.each do |survivor|
       name = survivor.css("a").attribute("title").text
-      survivors << name
+      bio_link = survivor.css('a').attribute('href').value
+      bio_page = Nokogiri::HTML(open("https://deadbydaylight.gamepedia.com#{bio_link}"))
+      bio = bio_page.css('div.mw-parser-output i').each do |line|
+        text = line.text.strip
+        if text.length > 300
+          survivor_info = {:name => name, :bio => text}
+          survivors << survivor_info
+        end
+      end
     end
-    survivors
-  end
+    survivor_hash = survivors.group_by {|h1| h1[:name]}.map do |k, v|
+      {:name => k, :bio => v.map {|h2| h2[:bio] }.join}
+    end
+survivor_hash
+end
+
+
 
   def self.scrape_killers
     page = Nokogiri::HTML(open("https://deadbydaylight.gamepedia.com/Dead_by_Daylight_Wiki"))
@@ -45,4 +58,5 @@ class Scraper
       end
     end
   end
+scrape_survivors
 end
