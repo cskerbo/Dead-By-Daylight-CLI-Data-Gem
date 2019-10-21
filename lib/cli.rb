714997f9a457c0d99c4488422064b7a7ac19a6df
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-21 21:59:09 +0000

    Combined character selections into one method

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,18 +14,14 @@ class CLI
      information, you can create your own custom character loadout. Try it now!\n
      Please select either survivor or killer."
     puts "1. Survivor\n".colorize(:blue) + "2. Killer".colorize(:red)
-    user_input = gets.strip
-    if user_input == "1"
-      display_all_survivors
-      puts "Enter the number of the survivor you would like to learn about:"
-      survivor_selection = gets.strip
-      display_selected_survivor(survivor_selection)
-    elsif user_input == "2"
-      display_all_killers
-      puts "Enter the number of the survivor you would like to learn about:"
-      killer_selection = gets.strip
-      display_selected_killer(killer_selection)
+    selection = gets.strip
+    confirmation = "N"
+    while confirmation == "N"
+      display_character_selection(selection)
+      puts "Would you like to select this character? Enter 'Y' to confirm, 'N' to go back."
+      confirmation = gets.strip
     end
+    puts "Accepted"
   end
 
   def make_survivors
@@ -45,20 +41,27 @@ class CLI
     end
   end
 
-  def display_selected_survivor(survivor_selection)
-    Survivor.all.each.with_index do |survivor, index|
-      if survivor_selection.to_i - 1 == index
-        puts "Name:" + " #{survivor.name}".colorize(:blue)
-        puts "Bio:" + " #{survivor.bio}".colorize(:green)
+  def display_character_selection(selection)
+    final_selection = "N"
+    if selection == "1"
+      display_all_survivors
+      puts "Enter the number of the survivor you would like to learn more about:"
+      survivor_selection = gets.strip
+      Survivor.all.each.with_index do |survivor, index|
+        if survivor_selection.to_i - 1 == index
+          puts "Name:" + " #{survivor.name}".colorize(:blue)
+          puts "Bio:" + " #{survivor.bio}".colorize(:green)
+        end
       end
-    end
-  end
-
-  def display_selected_killer(killer_selection)
-    Killer.all.each.with_index do |killer, index|
-      if killer_selection.to_i - 1 == index
-        puts "Name:" + " #{killer.name}".colorize(:red)
-        puts "Bio:" + " #{killer.bio}".colorize(:green)
+    elsif selection == "2"
+      display_all_killers
+      puts "Enter the number of the killer you would like to learn more about:"
+      killer_selection = gets.strip
+      Killer.all.each.with_index do |killer, index|
+        if killer_selection.to_i - 1 == index
+          puts "Name:" + " #{killer.name}".colorize(:red)
+          puts "Bio:" + " #{killer.bio}".colorize(:green)
+        end
       end
     end
   end
@@ -67,7 +70,6 @@ class CLI
     Killer.all.each.with_index do |killer, index|
       puts "#{index + 1}." + " #{killer.name}".colorize(:red)
       puts "---------------------".colorize(:green)
-
     end
   end
 end
